e33306d8b5570609a7b82abd1c9be60cc445caf6
RobertAndreiGit <robert_amarandei@yahoo.com>
2020-07-24 13:47:50 +0300

    Add Redux for state management

--- frontend/src/components/HomeScreen/HomeScreen.js
+++ frontend/src/components/HomeScreen/HomeScreen.js
@@ -1,37 +1,37 @@
 import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom'
 import axios from 'axios';
+
+import { listProducts } from '../../actions/productActions';
 
 import './HomeScreen.css';
 
 export default function HomeScreen(props) {
-    const [products, setProducts] = useState([]);
-
+    const productList = useSelector(state => state.productList);
+    const { products, loading, error } = productList;
+    const dispatch = useDispatch();
     useEffect(() => {
-        const fetchData = async () => {
-            const { data } = await axios.get("/api/products");
-            setProducts(data);
-        }
-        fetchData();
+        dispatch(listProducts());
     }, [])
 
-    return (
-        <ul className="products">
-            {products.map(product =>
-                <li>
-                    <div className="product">
-                        <Link to={`/product/${product._id}`}>
-                            <img className="product-image" src={product.imagePath} alt="" />
-                        </Link>
-                        <div className="product-name">
-                            <Link to={`/product/${product._id}`}>{product.name}</Link>
+    return loading ? <div>Loading...</div> :
+        error ? <div>{error}</div> :
+            <ul className="products">
+                {products.map(product =>
+                    <li>
+                        <div className="product">
+                            <Link to={`/product/${product._id}`}>
+                                <img className="product-image" src={product.imagePath} alt="" />
+                            </Link>
+                            <div className="product-name">
+                                <Link to={`/product/${product._id}`}>{product.name}</Link>
+                            </div>
+                            <div className="product-brand">{product.brand}</div>
+                            <div className="product-price">${product.price}</div>
+                            <div className="product-rating">{product.rating} Stars ({product.reviewCount} reviews)</div>
                         </div>
-                        <div className="product-brand">{product.brand}</div>
-                        <div className="product-price">${product.price}</div>
-                        <div className="product-rating">{product.rating} Stars ({product.reviewCount} reviews)</div>
-                    </div>
-                </li>
-            )}
-        </ul>
-    )
+                    </li>
+                )}
+            </ul>
 }
