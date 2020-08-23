3c2a09a4d818760ff535e8d91b1681a31fdb942e
Robert <robert_amarandei@yahoo.com>
2020-08-23 20:13:40 +0300

    MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import data from './data';
+import config from './config';
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).catch(err => console.log(err.reason));
 
 const app = express();
 const PORT = 5000;
 
+app.use("/api/users", userRoute);
 app.get("/api/products", (req, res) => {
     res.send(data.products);
 });
