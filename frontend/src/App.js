02c4086a5568f5a42a98802f9499d1c9aaca0104
Robert <robert_amarandei@yahoo.com>
2020-08-23 16:33:31 +0300

    Bug Fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,52 +4,54 @@ import './App.css';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import HomeScreen from './components/HomeScreen/HomeScreen';
 import ProductScreen from './components/ProductScreen/ProductScreen';
+import CartScreen from './components/CartScreen/CartScreen';
 
 function App() {
-  function openMenu() {
-    document.querySelector(".sidebar").classList.add("open");
-  }
+	function openMenu() {
+		document.querySelector('.sidebar').classList.add('open');
+	}
 
-  function closeMenu() {
-    document.querySelector(".sidebar").classList.remove("open");
-  }
-  return (
-    <BrowserRouter>
-      <div className="grid-container">
-        <header className="header">
-          <div className="brand">
-            <button onClick={openMenu}>
-              &#9776;
-                </button>
-            <Link to="/">🅱️abazon</Link>
-          </div>
-          <div className="header-links">
-            <a href="cart.hmtl">Cart</a>
-            <a href="login.html">Log In</a>
-          </div>
-        </header>
-        <aside className="sidebar">
-          <h3>Shopping Categories</h3>
-          <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-          <ul>
-            <li>
-              <a href="index.html">Pants</a>
-            </li>
-            <li>
-              <a href="index.html">Shirts</a>
-            </li>
-          </ul>
-        </aside>
-        <main className="main">
-          <div className="content">
-            <Route path="/product/:id" component={ProductScreen} />
-            <Route path="/" exact={true} component={HomeScreen} />
-          </div>
-        </main>
-        <footer className="footer">All rights reservered.</footer>
-      </div>
-    </BrowserRouter >
-  );
+	function closeMenu() {
+		document.querySelector('.sidebar').classList.remove('open');
+	}
+	return (
+		<BrowserRouter>
+			<div className="grid-container">
+				<header className="header">
+					<div className="brand">
+						<button onClick={openMenu}>&#9776;</button>
+						<Link to="/">🅱️abazon</Link>
+					</div>
+					<div className="header-links">
+						<Link to="/cart">Cart</Link>
+						<Link to="/login">Log In</Link>
+					</div>
+				</header>
+				<aside className="sidebar">
+					<h3>Shopping Categories</h3>
+					<button className="sidebar-close-button" onClick={closeMenu}>
+						x
+					</button>
+					<ul>
+						<li>
+							<a href="index.html">Pants</a>
+						</li>
+						<li>
+							<a href="index.html">Shirts</a>
+						</li>
+					</ul>
+				</aside>
+				<main className="main">
+					<div className="content">
+						<Route path="/product/:id" component={ProductScreen} />
+						<Route path="/cart/:id?" component={CartScreen} />
+						<Route path="/" exact={true} component={HomeScreen} />
+					</div>
+				</main>
+				<footer className="footer">All rights reservered.</footer>
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
--- frontend/src/components/ProductScreen/ProductScreen.js
+++ frontend/src/components/ProductScreen/ProductScreen.js
@@ -7,61 +7,79 @@ import { detailsProduct } from '../../actions/productActions';
 import './ProductScreen.css';
 
 function ProductScreen(props) {
-    const [qty, setQty] = useState(1);
-    const productDetails = useSelector(state => state.productDetails);
-    const { product, loading, error } = productDetails;
-    const dispatch = useDispatch();
+	const [ qty, setQty ] = useState(1);
+	const productDetails = useSelector((state) => state.productDetails);
+	const { product, loading, error } = productDetails;
+	const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(detailsProduct(props.match.params.id));
-    }, []);
+	useEffect(() => {
+		dispatch(detailsProduct(props.match.params.id));
+	}, []);
 
-    const handleAddToCart = () => {
-        props.history.push(`/cart/${props.match.params.id}?qty=${qty}`);
-    }
+	const handleAddToCart = () => {
+		props.history.push(`/cart/${props.match.params.id}?qty=${qty}`);
+	};
 
-    return <div>
-        <div className="back-to-result">
-            <Link to="/">Back to results</Link>
-        </div>
-        {loading ? <div>Loading...</div> :
-            error ? <div>{error}</div> :
-                <div className="details">
-                    <div className="details-image">
-                        <img src={product.imagePath} alt=""></img>
-                    </div>
-                    <div className="details-info">
-                        <ul>
-                            <li><h4>{product.name}</h4></li>
-                            <li>{product.rating} Stars ({product.reviewCount} reviews)</li>
-                            <li>Price: <b>${product.price}</b></li>
-                            <li>
-                                Description:
-                        <div>
-                                    {product.description}
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="details-action">
-                        <ul>
-                            <li>Price: {product.price}</li>
-                            <li>Status: {product.countInStock > 0 ? "In" : "Out Of"} Stock</li>
-                            <li>Qty:
-                                <select value={qty} onChange={(e) => setQty(e.target.value)}>
-                                    {[...Array(product.countInStock).keys()].map(x =>
-                                        <option value={x + 1} key={x + 1}>{x + 1}</option>
-                                    )}
-                                </select>
-                            </li>
-                            <li>
-                                <button disabled={product.countInStock === 0} onClick={handleAddToCart} className="button primary">Add to cart</button>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-        }
-    </div >
+	return (
+		<div>
+			<div className="back-to-result">
+				<Link to="/">Back to results</Link>
+			</div>
+			{loading ? (
+				<div>Loading...</div>
+			) : error ? (
+				<div>{error}</div>
+			) : (
+				<div className="details">
+					<div className="details-image">
+						<img src={product.imagePath} alt="" />
+					</div>
+					<div className="details-info">
+						<ul>
+							<li>
+								<h4>{product.name}</h4>
+							</li>
+							<li>
+								{product.rating} Stars ({product.reviewCount} reviews)
+							</li>
+							<li>
+								Price: <b>${product.price}</b>
+							</li>
+							<li>
+								Description:
+								<div>{product.description}</div>
+							</li>
+						</ul>
+					</div>
+					<div className="details-action">
+						<ul>
+							<li>Price: {product.price}</li>
+							<li>Status: {product.countInStock > 0 ? 'In' : 'Out Of'} Stock</li>
+							<li>
+								Qty:
+								<select value={qty} onChange={(e) => setQty(e.target.value)}>
+									{[ ...Array(product.countInStock).keys() ].map((x) => (
+										<option value={x + 1} key={x + 1}>
+											{x + 1}
+										</option>
+									))}
+								</select>
+							</li>
+							<li>
+								<button
+									disabled={product.countInStock === 0}
+									onClick={handleAddToCart}
+									className="button primary"
+								>
+									Add to cart
+								</button>
+							</li>
+						</ul>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default ProductScreen;
